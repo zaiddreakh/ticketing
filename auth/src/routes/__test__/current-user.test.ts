e0cb8dc3ce7bab0c60ea3d8d61563053be3bbb26
Zaid Dreakh <zaid.dreakh@hotmail.co.uk>
2021-01-01 18:08:29 +0100

    breaking tests test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -3,13 +3,12 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about current user', async () => {
-    const cookie = await global.signin();
+  const cookie = await global.signin();
 
   const res = await request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
-    .send()
-    .expect(200);
+    .send();
 
   expect(res.body.currentUser.email).toEqual('zaid@test.com');
 });
@@ -20,5 +19,5 @@ it('responds with null when not authenticated', async () => {
     .send()
     .expect(200);
 
-    expect(res.body.currentUser).toEqual(null);
+  expect(res.body.currentUser).toEqual(null);
 });
