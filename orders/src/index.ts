46ed63b396620368afb6c08132d66cc71695148f
Zaid Dreakh <zaid.dreakh@hotmail.co.uk>
2021-01-01 18:24:05 +0100

    startup message for orders

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,6 +8,7 @@ import { TicketUpdatedListener } from './events/listeners/ticket-updated-listene
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
+  console.log('starting!');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
