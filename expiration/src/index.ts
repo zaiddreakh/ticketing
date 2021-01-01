dc458e4802ebd1dd3e078259029d6babaa79b287
Zaid Dreakh <zaid.dreakh@hotmail.co.uk>
2021-01-01 22:12:23 +0100

    added logs to test new github workflows

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,7 @@ import { orderCreatedListener } from './events/listeners/order-created-listener'
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
+  console.log('starting service!');
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error('NATS_CLUSTER_ID must be defined');
   }
